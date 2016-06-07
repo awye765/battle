a598d731d91ab24d74a9040022fe15b922ebb0d7
awye765 <awye765@googlemail.com>
2016-06-07 16:44:10 +0100

    Refactoring to avoid rendering a view directly from a POST request re names

--- app.rb
+++ app.rb
@@ -1,15 +1,26 @@
+
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    p session[:player_1_name] = params[:player_1_name]
+    p session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    p @player_1_name = session[:player_1_name]
+    p @player_2_name = session[:player_2_name]
     erb(:play)
   end
+
    # start the server if ruby file executed directly
   run! if app_file == $0
 end
